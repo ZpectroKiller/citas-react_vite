28637304f901c34d394f6eab85006d372b324d86
Bruno Valadez <zpectroplay@gmail.com>
2023-08-16 09:38:17 -0600

    Primer Commit

---
+++ src/components/Error.jsx
@@ -0,0 +1,12 @@
+//rafce
+
+const Error = ({children}) => {
+  return (
+    <div className='bg-red-700 text-white text-center p-3 font-bold mb-3 uppercase rounded-md'>
+        {children}  
+    </div>
+  )
+}
+
+export default Error
+
